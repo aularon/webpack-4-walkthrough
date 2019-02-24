2f8cdb3c32389dc00114a0b08d4693b717e25bbd
Hasan Arous <arous.hasan@gmail.com>
2019-02-24 11:40:24 +0200

    2.6 wrap up / cleanup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }
