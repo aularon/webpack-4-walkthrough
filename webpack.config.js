7bcfdf563fe1442e86c8d3c96e80c0c781f50dad
Hasan Arous <arous.hasan@gmail.com>
2019-02-24 12:44:03 +0200

    4.1 using source maps

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,11 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
   plugins: [
-    new CleanWebpackPlugin(['dist/*']),
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
+      title: 'Development'
     })
   ],
   output: {
